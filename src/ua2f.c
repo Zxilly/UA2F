c446af7f770611bf9c86b699c3b92173bbab7739
zxilly <zhouxinyu1001@gmail.com>
2020-12-05 15:29:18 +0800

    update

--- src/ua2f.c
+++ src/ua2f.c
@@ -1,21 +1,18 @@
 //#include <errno.h>
+//#include <wait.h>
+//#include <sys/param.h>
+//#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
 #include <syslog.h>
-//#include <wait.h>
 #include <signal.h>
-//#include <sys/param.h>
-//#include <sys/stat.h>
 #include <arpa/inet.h>
-
 #include <libmnl/libmnl.h>
 #include <linux/netfilter.h>
-
 #include <linux/netfilter/nfnetlink_queue.h>
-
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include <libnetfilter_queue/libnetfilter_queue_tcp.h>
 #include <libnetfilter_queue/libnetfilter_queue_ipv4.h>
