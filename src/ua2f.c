a63eb052bbafbed4ee7839a613f4e00011cbc898
Zxilly <zxilly@outlook.com>
2024-12-15 21:32:05 +0800

    build: fix link

--- src/ua2f.c
+++ src/ua2f.c
@@ -24,25 +24,21 @@ void signal_handler(const int signum) {
     should_exit = true;
 }
 
-int parse_packet(const struct nf_queue *queue, struct nf_buffer *buf) {
-    struct nf_packet packet[1] = {0};
-
-    while (!should_exit) {
-        const __auto_type status = nfqueue_next(buf, packet);
-        if (status == IO_READY) {
-            handle_packet(queue, packet);
-        } else {
-            return status;
-        }
-    }
-
-    return IO_ERROR;
-}
-
 int read_buffer(struct nf_queue *queue, struct nf_buffer *buf) {
     const __auto_type buf_status = nfqueue_receive(queue, buf, 0);
     if (buf_status == IO_READY) {
-        return parse_packet(queue, buf);
+        struct nf_packet packet[1] = {0};
+
+        while (!should_exit) {
+            const __auto_type status = nfqueue_next(buf, packet);
+            if (status == IO_READY) {
+                handle_packet(queue, packet);
+            } else {
+                return status;
+            }
+        }
+
+        return IO_ERROR;
     }
     return buf_status;
 }
